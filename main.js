1d4926ba06b4e9ab4af4a55dc0923809c5ee16e6
helloPandaZilong <93321068+helloPandaZilong@users.noreply.github.com>
2022-02-21 14:32:02 +0900

    JS : Home contact button

--- main.js
+++ main.js
@@ -15,15 +15,22 @@ document.addEventListener("scroll", () => {
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
-  console.log(event.target.dataset.link);
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
 
-  console.log(event.target.dataset.link);
+  scrollIntoView(link);
+});
 
+// Handle click on "contact me" button on home
+const contactMe = document.querySelector(".home__contact");
+contactMe.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
   const scrollTo = document.querySelector(link);
   scrollTo.scrollIntoView({ behavior: "smooth" });
-});
+}
