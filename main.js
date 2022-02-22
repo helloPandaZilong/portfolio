54e7e4746e38179db48a1c9433d4103dcc2e8989
helloPandaZilong <93321068+helloPandaZilong@users.noreply.github.com>
2022-02-22 13:34:32 +0900

    HTML : Update

--- main.js
+++ main.js
@@ -79,7 +79,6 @@ workBtnContainer.addEventListener("click", (e) => {
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
-      console.log(project.dataset.type);
       if (filter === "*" || filter === project.dataset.type) {
         project.classList.remove("invisible");
       } else {
